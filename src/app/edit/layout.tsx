50e8dca69453610e5510ea0c6e52c8c30f240520
Jeysef <josimich7@gmail.com>
2024-01-06 22:21:38 +0100

    visual

--- src/app/edit/layout.tsx
+++ src/app/edit/layout.tsx
@@ -1,3 +1,10 @@
+import { SiteHeader } from "@/components/site-header";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <div className="container relative">{children}</div>;
+    return (
+        <div className="h-full w-full">
+            <SiteHeader />
+            <div className="container relative">{children}</div>
+        </div>
+    );
 }
