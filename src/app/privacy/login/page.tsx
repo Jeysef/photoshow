2fc2d6ae93a0961ba91af25bd0cf2ddb53b8195b
Jeysef <josimich7@gmail.com>
2024-01-12 18:17:40 +0100

    Add padding to login page paragraph

--- src/app/privacy/login/page.tsx
+++ src/app/privacy/login/page.tsx
@@ -3,7 +3,7 @@ import Center from "@/components/center";
 export default function Page() {
     return (
         <Center>
-            <p>
+            <p className="px-4">
                 Please don't sue me.
                 <br /> I do not guarantee safety of your informations. If you don't accept that, please do not use my site
             </p>
