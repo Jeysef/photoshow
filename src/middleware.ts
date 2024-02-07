79721256c69450b920ab9b28b28f9d64af528032
Jeysef <josimich7@gmail.com>
2024-02-07 09:36:48 +0100

    test: disable clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
     publicRoutes: ["/", "/privacy/(.*)", "/api/uploadthing"],
-    ignoredRoutes: ["favicon.ico", "robots.txt", "sitemap.xml", "og.png"],
+    ignoredRoutes: ["favicon.ico", "robots.txt", "sitemap.xml", "og.png", "(.*)"],
+
 });
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: [],
 };
